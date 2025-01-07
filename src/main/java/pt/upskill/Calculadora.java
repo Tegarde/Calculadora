354a708546d26eb54dd9f2ec1d850c2ce7cd4b5e
Viviane Almeida <viviane.aalmeida@live.com>
2025-01-07 15:33:14 +0000

    add method calcularProduto

--- src/main/java/pt/upskill/Calculadora.java
+++ src/main/java/pt/upskill/Calculadora.java
@@ -1,4 +1,11 @@
 package pt.upskill;
 
 public class Calculadora {
+
+    double resultado;
+
+    public double calcularProduto(double num1, double num2) {
+        resultado = num1 * num2;
+        return resultado;
+    }
 }
