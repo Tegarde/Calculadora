bdedaa940d22527452c87134774c33dc6152f184
Ricardo Cerqueira <d3vigan31@gmail.com>
2025-01-07 16:17:31 +0000

    Decimal to binary added

--- src/main/java/pt/upskill/domain/Calculadora.java
+++ src/main/java/pt/upskill/domain/Calculadora.java
@@ -82,4 +82,8 @@ public class Calculadora {
         return sum == originalNum;
     }
 
+    public String decimalToBinary(int decimal) {
+        return Integer.toBinaryString(decimal);
+    }
+
 }
