c919c8d3822ea772e8b3e9de858e6b47593bb4a8
DR-dev8 <diana8dev@gmail.com>
2025-01-07 16:10:44 +0000

    Add sum tests

---
+++ src/test/java/pt/upskill/domain/CalculadoraTest.java
@@ -0,0 +1,62 @@
+package pt.upskill.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculadoraTest {
+    Calculadora calc = new Calculadora();
+
+    @Test
+    void sumTwoNumbers() {
+        double result = calc.sum(5, 3);
+        assertEquals(8.0, result, 0.01);
+    }
+
+    @Test
+    void sumWithStoreResult() {
+        calc.save(10);
+        double result = calc.sum(5.0);
+        assertEquals(15.0, result, 0.01);
+    }
+
+    @Test
+    void subtraction() {
+    }
+
+    @Test
+    void testSubtraction() {
+    }
+
+    @Test
+    void calculateProduct() {
+    }
+
+    @Test
+    void testCalculateProduct() {
+    }
+
+    @Test
+    void division() {
+    }
+
+    @Test
+    void testDivision() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void erase() {
+    }
+
+    @Test
+    void power() {
+    }
+
+    @Test
+    void testPower() {
+    }
+}
