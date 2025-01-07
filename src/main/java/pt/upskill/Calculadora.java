d93264c2490fa69b6fa39fb8ee951cb587cc0116
Alberto Rosa <albertorosa@gmail.com>
2025-01-07 15:40:10 +0000

    Add power method

--- src/main/java/pt/upskill/Calculadora.java
+++ src/main/java/pt/upskill/Calculadora.java
@@ -10,25 +10,34 @@ public class Calculadora {
     public double subtraction(double num1, int num2) {
         return num1 - num2;
     }
+
     public double subtraction(double num1) {
         return result - num1;
     }
 
-    public double division (double num1, double num2) {
+    public double division(double num1, double num2) {
         if (num2 == 0) {
             throw new IllegalArgumentException("Cannot divide by zero");
         }
         return num1 / num2;
     }
 
-    public double division (double num1) {
+    public double division(double num1) {
         if (num1 == 0) {
             throw new IllegalArgumentException("Cannot divide by zero");
         }
         return result / num1;
     }
 
-    public void save(double result){
+    public void save(double result) {
         this.result = result;
+    }
+
+    public double power(double x, double y) {
+        return Math.pow(x, y);
+    }
+
+    public double power(double x) {
+        return Math.pow(result, x);
     }
 }
