0a8787c60cc685b92f8ebef8b769771a41318caf
DR-dev8 <diana8dev@gmail.com>
2025-01-07 16:15:37 +0000

    Add subtraction tests

--- src/test/java/pt/upskill/domain/CalculadoraTest.java
+++ src/test/java/pt/upskill/domain/CalculadoraTest.java
@@ -10,22 +10,38 @@ class CalculadoraTest {
     @Test
     void sumTwoNumbers() {
         double result = calc.sum(5, 3);
-        assertEquals(8.0, result, 0.01);
+        double expected = 8;
+        assertEquals(expected, result);
     }
 
     @Test
     void sumWithStoreResult() {
         calc.save(10);
         double result = calc.sum(5.0);
-        assertEquals(15.0, result, 0.01);
+        double expected = 15;
+        assertEquals(expected, result);
     }
 
     @Test
-    void subtraction() {
+    void subtractionTwoNumbers() {
+        double result = calc.subtraction(5, 3);
+        double expected = 2;
+        assertEquals(expected, result);
     }
 
     @Test
-    void testSubtraction() {
+    void subtractionTwoNumbersWithFirstNumberLessThanSecond() {
+        double result = calc.subtraction(3, 5);
+        double expected = -2;
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void subtractionWithStoreResult() {
+        calc.save(10);
+        double result = calc.subtraction(5);
+        double expected = 5;
+        assertEquals(expected, result);
     }
 
     @Test
